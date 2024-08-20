93feea236c23993307e7026b4f9f460d8b06d551
joao daniel <joaodaniel160403@gmail.com>
2024-08-20 00:26:08 -0300

    classe de modulo

---
+++ primeiro-programa-java/src/cursoJava/executavel/Modulo.java
@@ -0,0 +1,13 @@
+package cursoJava.executavel;
+
+public class Modulo {
+	public static void main(String[] args) {
+		
+		int carros = 9;
+		int pessoas = 2;
+		int resto = carros % pessoas; 
+		System.out.println("total de carros que restou: " + resto);
+		
+		
+	}
+}
